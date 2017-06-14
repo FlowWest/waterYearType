b2a1f71760192b72f14b857fef504371326ae017
thesadie <sgill@flowwest.com>
2017-06-14 12:24:53 -0700

    started documentation

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Water Year Index and Type 1901-2016
+#'
+#' @description blaw
+#'
+#' @format A data frame with 227 rows and 7 variables:
+#' \describe{
+#' \item{WY}{October-September}
+#' \item{Oct_Mar}{}
+#' \item{Apr_Jul}{}
+#' \item{WYsum}{}
+#' \item{Index}{}
+#' \item{Yr_type}{}
+#' \item{location}{Sacramento Valley or San Joaquin Valley}
+#' }
+#'
+#' @source
+#'
+'water_years'
--- data-raw/clean_wy.R
+++ data-raw/clean_wy.R
@@ -15,13 +15,32 @@ parse_wy <- function(x) {
 
 wy_parsed <- purrr::map_df(wy, parse_wy)
 
-dplyr::glimpse(wy_parsed)
+sac <- dplyr::mutate(wy_parsed[ , 1:6], location = 'Sacramento Valley')
 
-sac <- wy_parsed[ , 1:6] %>%
-  dplyr::mutate(location = 'Sacramento Valley')
-
-san_joaquin <- wy_parsed[ , c(1, 7:11)] %>%
-  dplyr::mutate(location = 'San Joaquin Valley')
+san_joaquin <- dplyr::mutate(wy_parsed[ , c(1, 7:11)], location = 'San Joaquin Valley')
 names(san_joaquin) <- stringr::str_replace(names(san_joaquin), 'SJ_', '')
 
-both <- bind_rows(sac, san_joaquin)
+#add 1901-1905 for san joaquin
+extra_sj <- readr::read_lines(
+'1901   3.49    5.58    9.39   4.60     W
+1902    1.12    3.81    5.08   3.41    AN
+1903    1.45    4.13    5.71   3.45    AN
+1904    1.96    5.37    7.64   4.31     W
+1905    1.82    3.36    5.30   3.24    AN')
+
+parse_extra <- function(x) {
+
+  temp <- unlist(stringr::str_split(x, ' '))
+  temp2 <- temp[which(temp!= '')]
+
+  temp3 <- dplyr::data_frame(WY = temp2[1], Oct_Mar = temp2[2], Apr_Jul = temp2[3],
+                             WYsum = temp2[4], Index = temp2[5], Yr_type = temp2[6], location = 'San Joaquin Valley')
+
+  return(temp3)
+}
+
+extra_parsed <- purrr::map_df(extra_sj, parse_extra)
+
+water_years <- dplyr::bind_rows(sac, extra_parsed, san_joaquin)
+
+devtools::use_data(water_years)
